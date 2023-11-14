b324fc84dc2abdb50046d5891b6f0498a10049f8
miru <stkssc002@gmail.com>
2023-11-14 22:26:57 +0700

    done ui, ux scene

--- src/components/Widget/Scene/LoadScene.vue
+++ src/components/Widget/Scene/LoadScene.vue
@@ -3,11 +3,16 @@
     <n-collapse>
       <n-collapse-item v-for="num in numTouch"
         :title="`Công tắc ${num} - khoá điều khiển ${scenes[num]?.value ? scenes[num]?.value.length : 0}`" :name="num">
-        <n-space v-if="scenes[num]?.value" vertical>
+        <n-space v-if="scenes[num]?.value && scenes[num]?.value.length > 0" vertical>
           <n-card v-for="scene in scenes[num].value" :title="`Địa chỉ: ${scene.target}`" size="small">
+            <template #header-extra>
+              <n-button @click="removeScene(scene, num)" type="error" round>
+                Xoá
+              </n-button>
+            </template>
             <span><b>Loại thiết bị:</b> {{ scene.payload.dType }}</span>
             <div>
-              <p><b>Chế độ điều khiển:</b> {{ scene.payload.mode === 1 ? 'chỉ một' : 'tất cả' }}</p>
+              <p><b>Chế độ điều khiển:</b> {{ scene.payload.mode === 1 ? 'tất cả' : 'chỉ một' }}</p>
               <p v-if="scene.payload.mode === 1"><b>Vị trí:</b> chạm {{ scene.payload.position }}</p>
             </div>
             <span><b>Trạng thái:</b> {{ scene.payload.state ? 'bật' : 'tắt' }}</span>
@@ -51,23 +56,55 @@ const props = defineProps<{
 const numTouch = ref<number>(parseInt(props.dType.slice(-1)));
 let scenes = reactive<SceneProps>({});
 
-Array(numTouch).fill(0).forEach((_, i) => {
+Array(numTouch.value).fill(0).forEach((_, i) => {
   scenes[i + 1] = { value: [] };
 })
 
+const removeScene = (scene: any, position: number) => {
+  // console.log(position);
+  if (wsClient.value) {
+    if (wsClient.value.readyState === wsClient.value.OPEN) {
+      const packet = {
+        target: props.target,
+        payload: {
+          pType: 'scene',
+          pAction: 'DELETE',
+          mode: 1,
+          position: position,
+          id: scene.id
+        }
+      };
+      wsClient.value.send(JSON.stringify(packet));
+    }
+  }
+}
+
 const onMessage = (event: MessageEvent<any>) => {
-  const { target, payload }: MessageSocketProps = JSON.parse(typeof event.data === 'string' ? event.data : '');
+  const { target, payload } = JSON.parse(typeof event.data === 'string' ? event.data : '');
 
   if (target !== props.target) { // check device id of payload
     return;
   }
 
   if (payload) {
-    const { payload: scene, pType, position } = payload;
-    if (pType !== 'scene' || scene === null) {
+    const { payload: scene, message, position, pType, id } = payload;
+
+    if (pType !== 'scene') {
       return;
     }
-    if (position <= numTouch.value && position > 0) {
+
+    console.log(payload);
+
+    if (message === 'SCENE_CREATE_OK') {
+      notyf.success(`Thêm ngữ cảnh vào chạm ${position} thành công!`);
+    } else if (message === 'SCENE_DELETE_OK') {
+      notyf.success(`Xoá ngữ cảnh thành công!`);
+      if (position > 0) {
+        scenes[position]['value'] = scenes[position]['value'].filter((scene: any) => scene.id !== id);
+      }
+    }
+
+    if (position <= numTouch.value && position > 0 && scene) {
       scenes[position]['value'].push(scene);
     }
     console.log(scene);
--- src/components/Widget/Scene/ModalCreate.vue
+++ src/components/Widget/Scene/ModalCreate.vue
@@ -1,28 +1,80 @@
 <template>
-  <n-drawer v-model:show="showDrawer" default-width="100%" :placement="'right'" :on-after-leave="() => { emit('close') }">
+  <n-drawer v-model:show="showDrawer" default-width="100%" height="60%" :placement="'bottom'"
+    :on-after-leave="() => { emit('close') }">
     <n-drawer-content title="Tạo ngữ cảnh" closable>
       <n-space vertical>
-        <n-text strong>
-          Chọn công tắc
-        </n-text>
-        <n-select v-model:value="selectPosition" :options="options" />
+        <n-space vertical>
+          <n-text strong>
+            Ràng buộc vào
+          </n-text>
+          <n-select v-model:value="numTouch" :options="bindOptions" />
+        </n-space>
+        <n-space vertical>
+          <n-text strong>
+            Chọn loại công tắc
+          </n-text>
+          <n-select v-model:value="chooseDType" :options="optionsDType" />
+        </n-space>
+        <n-space vertical>
+          <n-text strong>
+            Chọn công tắc
+          </n-text>
+          <n-tree-select v-model:value="selectPosition" :options="touchOptions" check-strategy="child" />
+        </n-space>
+        <n-space vertical>
+          <n-text strong>
+            Chọn trạng thái
+          </n-text>
+          <n-switch v-model:value="chooseState" />
+        </n-space>
+        <n-space justify="end">
+          <n-button @click="addScene" type="success">
+            Thêm đối tượng điều khiển
+          </n-button>
+        </n-space>
       </n-space>
     </n-drawer-content>
   </n-drawer>
 </template>
 
 <script setup lang="ts">
-import { NDrawer, NDrawerContent, SelectOption, NSelect, NSpace, NText } from 'naive-ui';
+import { NDrawer, NDrawerContent, SelectOption, NSelect, NSpace, NText, TreeSelectOption, NTreeSelect, NSwitch, NButton } from 'naive-ui';
 
 import { storeToRefs } from 'pinia'
-import { useNodeStore } from '@/store'
+import { useNodeStore, useCommonStore } from '@/store'
 import { ref, onUnmounted, watch } from 'vue';
 
 import notyf from '@/notyf';
 
-import { MenuProps } from '@/components/Widget';
+import { MenuProps, WidgetType } from '@/components/Widget';
 
-const { value } = storeToRefs(useNodeStore());
+interface TouchProps {
+  label: string,
+  key: string | number,
+  children: Array<TouchProps | []>
+}
+
+const optionsDType = [
+  {
+    label: 'Công tắc 1 chạm',
+    value: 'touch_1'
+  },
+  {
+    label: 'Công tắc 2 chạm',
+    value: 'touch_2'
+  },
+  {
+    label: 'Công tắc 3 chạm',
+    value: 'touch_3'
+  },
+  {
+    label: 'Công tắc 4 chạm',
+    value: 'touch_4'
+  },
+]
+
+const { value: nodes } = storeToRefs(useNodeStore());
+const { wsClient } = storeToRefs(useCommonStore());
 
 const props = defineProps<{
   nodeBase: MenuProps,
@@ -33,19 +85,76 @@ const emit = defineEmits<{
   (e: 'close'): void
 }>();
 
+const dTypeNumTouch = ref<number>(parseInt(props.nodeBase.dType.slice(-1)));
+
+const chooseState = ref<boolean>(true);
+const chooseDType = ref<null | WidgetType>(null);
 const showDrawer = ref<boolean>(props.show);
-const numTouch = ref<number>(0);
-const selectPosition = ref<number | null>(null);
-const options = ref<Array<SelectOption> | []>([]);
+const numTouch = ref(null);
+const touchOptions = ref<Array<TreeSelectOption> | []>([]);
+const selectPosition = ref<string | null>(null);
+const bindOptions = ref<Array<SelectOption> | []>([]);
+
+const addScene = () => {
+  if (wsClient.value) {
+    if (wsClient.value.readyState === wsClient.value.OPEN) {
+      console.log(numTouch.value, chooseDType.value, selectPosition.value, chooseState.value);
+
+      if (!numTouch.value || !selectPosition.value || !chooseDType.value) {
+        notyf.error('Vui lòng chọn đầy đủ thông tin');
+        return;
+      }
+
+      const packet = {
+        target: props.nodeBase.id,
+        payload: {
+          pType: 'scene',
+          pAction: 'CREATE',
+          mode: 1, // read all scene at num
+          position: numTouch.value,
+          payload: {
+            target: selectPosition.value.split('-')[0],
+            payload: {
+              pType: "controll",
+              pAction: "UPDATE",
+              dType: chooseDType.value,
+              mode: 0,
+              position: parseInt(selectPosition.value.split('-')[1]),
+              state: chooseState.value
+            }
+          }
+        }
+      };
+      wsClient.value.send(JSON.stringify(packet));
+    }
+  }
+}
 
 watch(props, (propsCtx) => {
-  numTouch.value = parseInt(propsCtx.nodeBase.dType.slice(-1));
+  dTypeNumTouch.value = parseInt(propsCtx.nodeBase.dType.slice(-1));
   showDrawer.value = propsCtx.show;
 })
 
-watch(numTouch, (num) => {
-  options.value = Array(num).fill(0).map((_, index) => ({ label: `Chạm ${index + 1}`, value: index + 1 }));
-  console.log(options.value);
+const filterOptionDType = (num: number) => {
+  bindOptions.value = Array(num).fill(0).map((_, index) => ({ label: `Chạm ${index + 1}`, value: index + 1 }));
+}
+
+filterOptionDType(dTypeNumTouch.value);
+
+watch(dTypeNumTouch, filterOptionDType);
+
+watch(chooseDType, (dType) => {
+  if (dType) {
+    touchOptions.value = nodes.value.filter((node) => node.info.dType === dType).map((node) => ({
+      label: `${node.target}`,
+      key: `${node.target}`,
+      children: node.info.dType.includes('touch') ? node.value.map(touch => ({
+        label: `Chạm ${touch.position}`,
+        key: `${node.target}-${touch.position}`,
+      })) : [],
+    }));
+    console.log(touchOptions.value);
+  }
 })
 
 </script>
